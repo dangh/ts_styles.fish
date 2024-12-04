3f3b5ceca102cc7bc420f85794cce8e7d8f99f05
Dang <huynhminhdang@gmail.com>
2024-12-04 20:25:08 +0800

    Update ts_styles.fish

--- functions/ts_styles.fish
+++ functions/ts_styles.fish
@@ -1,25 +1,5 @@
 function ts_styles
-    set -l app
-    set -l light
-    set -l dark
-    set -l style
-
-    if set -q ZED_TERM
-        switch "$__CFBundleIdentifier"
-            case "dev.zed.Zed-Preview"
-                set app zed_preview
-            case "dev.zed.Zed"
-                set app zed
-        end
-    else if set -q ALACRITTY_WINDOW_ID
-        set app alacritty
-    end
-
-    set -l zed_preview bg=#f3edda bg=#14222c
-    set -l zed bg=#14222c
-    set -l alacritty bg=#2f383f
-
-    set -l style $$app
+    set -l style bg=#f3edda bg=#14222c
     set -l light_style $style[1]
     set -l dark_style $style[2]
     test -n "$dark_style" || set dark_style $light_style
